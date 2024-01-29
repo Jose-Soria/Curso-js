64f1e9940b92d04732b819e3a86b71ace2b3ac59
José Soria <sorialujanjm@gmail.com>
2024-01-29 19:31:53 -0300

    preentrega1

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const NUMERO = 8;
+
+for(let i= 0; i <= 10; i++){
+    const RESULTADO = NUMERO * i;
+    console.log(NUMERO + " x " + i + " = " + RESULTADO)
+}
+
+/* entrego este ejercicio que hicimos en clase para que no quede sin realizar la entrega. Volví a ver las cuatro clases que incluyen el contenido para esta entrega, pero no consigo interpretar de que manera tengo que aplicar los conocimientos que vamos adquiriendo. No entiendo como plantear los ciclos aplicados a una situación o problema particular a resolver. Estoy bastante perdido, pero voy a seguir tratando de entender como aplicar lo visto en clases. Desde ya me disculpo por hacerte perder el tiempo con esto, que ni siquiera es la preentrega que se supone que entregue.*/
