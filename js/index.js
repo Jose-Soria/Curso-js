c343ef2afd083b23beff99ade8e6d82266f91586
José Soria <sorialujanjm@gmail.com>
2024-02-06 01:13:59 -0300

    reentrega1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,49 @@
-const NUMERO = 8;
+/*const NUMERO = 8;
 
 for(let i= 0; i <= 10; i++){
     const RESULTADO = NUMERO * i;
     console.log(NUMERO + " x " + i + " = " + RESULTADO)
-}
+}*/
 
 /* entrego este ejercicio que hicimos en clase para que no quede sin realizar la entrega. Volví a ver las cuatro clases que incluyen el contenido para esta entrega, pero no consigo interpretar de que manera tengo que aplicar los conocimientos que vamos adquiriendo. No entiendo como plantear los ciclos aplicados a una situación o problema particular a resolver. Estoy bastante perdido, pero voy a seguir tratando de entender como aplicar lo visto en clases. Desde ya me disculpo por hacerte perder el tiempo con esto, que ni siquiera es la preentrega que se supone que entregue.*/
+
+
+
+
+
+/*    5/02/24 (reentrega)
+
+Hola Nico, entre los videos de las clases y algunos de youtube, creo que pude plantear algo más o menos correcto. Tomé uno de los ejemplos que recomendaba Coder en caso de no saber que hacer. Hice el ejemplo de "calcular la edad promedio entre los alumnos ingresados". Como a la variable "alumnos" le di un valor de 5, usé ese número para la cantidad de veces que ingresé las edades.
+Gracias por la paciencia y comprensión, espero tu devolución. */
+
+let alumnos = 5;
+let suma = 0;
+let seguir;
+
+
+
+do{
+    const EDAD = parseInt(prompt("Ingresar edad del alumno: "));
+    suma += EDAD;
+    seguir = prompt("Desea ingresar la edad de otro alumno?, si o no");
+
+    function promediar(){
+        let resultado;
+        resultado = suma / alumnos;
+        return resultado;
+        }  
+    
+    let promedio = promediar(suma,alumnos);
+    
+    if(seguir === "no"){
+        console.log("El promedio de edad entre los alumnos es de: " + promedio + " años.");
+        }
+
+}
+
+    
+while(seguir === "si"){
+    console.log(suma)
+}
+
+ 
